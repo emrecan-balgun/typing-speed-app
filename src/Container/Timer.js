13d11369c02b456f45359310156233924a65f397
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-25 15:46:31 +0300

    add react-countdown-clock and add to timer.js

--- src/Container/Timer.js
+++ src/Container/Timer.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import ReactCountdownClock from 'react-countdown-clock';
 
 function Timer() {
     return (
         <div className="timer">
-            
+            <ReactCountdownClock seconds={60}
+                     color="#000"
+                     alpha={0.9}
+                     size={60} 
+                    //  onComplete={handleCompleted} 
+            />
         </div>
     )
 }
