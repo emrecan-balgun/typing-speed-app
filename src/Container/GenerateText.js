d85c168c76f0a53d83b2ce091748f8101831fff3
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-26 00:21:08 +0300

    update GenerateText.js and sass files

--- src/Container/GenerateText.js
+++ src/Container/GenerateText.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function GenerateText() {
+    const text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum minus tempore dolor quaerat, doloribus beatae tenetur at? Sit quia necessitatibus labore, exercitationem laborum voluptas accusantium itaque nulla. Adipisci dicta quis pariatur assumenda ullam, sed deserunt in excepturi aperiam alias consequatur cumque mollitia ad dolores dolore aut quidem molestias soluta corporis.";
+
     return (
         <div className="generateText">
-            
+            <input className="generateText__input" type="text" value={text} />
         </div>
     )
 }
