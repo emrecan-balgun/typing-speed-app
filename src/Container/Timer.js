8f69148f9625ba747725d0005b4f7f48d8a6fa8d
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-25 16:31:18 +0300

    add react-countdown-hook and remove react-countdown-clock

--- src/Container/Timer.js
+++ src/Container/Timer.js
@@ -1,15 +1,8 @@
-import { useState, useEffect } from 'react'
-import ReactCountdownClock from 'react-countdown-clock';
 
 function Timer() {
     return (
         <div className="timer">
-            <ReactCountdownClock seconds={60}
-                     color="#000"
-                     alpha={0.9}
-                     size={60} 
-                    //  onComplete={handleCompleted} 
-            />
+            
         </div>
     )
 }
--- src/app/typingSlice.js
+++ src/app/typingSlice.js
@@ -5,7 +5,13 @@ export const typingSlice = createSlice({
   initialState: {
     seconds: 60,
   },
-  reducers: {}
+  reducers: {
+      resetSeconds: (state) => {
+          state.seconds = 60;
+      }
+  }
 })
 
+export const { resetSeconds } = typingSlice.actions;
+export const seconds = (state) => state.typing.seconds;
 export default typingSlice.reducer;
