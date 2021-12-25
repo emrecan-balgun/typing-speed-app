c56d12277f5bea1477e020ea65188582f6fd231a
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-26 00:24:02 +0300

    update EnterText.js and enterText.scss

--- src/Container/EnterText.js
+++ src/Container/EnterText.js
@@ -3,7 +3,7 @@ import React from 'react'
 function EnterText() {
     return (
         <div className="enterText">
-            
+            <input className="enterText__input" type="text" />
         </div>
     )
 }
