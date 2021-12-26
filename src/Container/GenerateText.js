e532fd0b377bcf61c99d8b855fb74dd617921f6e
Emrecan Balgün <deathlygrave@gmail.com>
2021-12-26 12:09:34 +0300

    add random-words, refactor GenerateText.js, update sass files

--- src/Container/GenerateText.js
+++ src/Container/GenerateText.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import randomWords from 'random-words';
+
+const NUMB_OF_WORDS = 200;
 
 function GenerateText() {
-    const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum minus";
+    const [words, setWords] = useState([])
+
+    useEffect(() => {
+        setWords(generateWords())
+    }, [])
+
+    function generateWords() {
+        return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords())
+    }
 
     return (
         <div className="generateText">
-            <input className="generateText__input" type="text" value={text}
-            disabled
-            />
+            <div className="generateText__text">
+                {words.map((word, i) => (
+                    <>
+                        <span>
+                            {word}
+                        </span>
+                        <span> </span>
+                    </>
+                ))}
+            </div>
         </div>
     )
 }
--- src/Container/Timer.js
+++ src/Container/Timer.js
@@ -3,7 +3,7 @@ function Timer() {
     return (
         <div className="timer">
             <h1 className="timer__time">{}</h1>
-            <button className="timer__button" onClick={}>Reset</button>
+            <button className="timer__button">Reset</button>
         </div>
     )
 }
